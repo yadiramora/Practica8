058c5737e584d689043a71424f1c77a0902402f7
yadiramora <almn.yadiracobos@cbtis66.edu.mx>
2022-10-12 19:55:45 -0500

    Practica8

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  mensaje='';
+
+  actualizar(t: number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
+}
---
+++ src/app/cronometro/cronometro.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-cronometro',
+  templateUrl: './cronometro.component.html',
+  styleUrls: ['./cronometro.component.css']
+})
+export class CronometroComponent implements OnInit {
+  segundo=0;
+  @Input() inicio: number=0;
+  @Output() multiplo10 = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+    this.segundo = this.inicio;
+    setInterval(() => {
+      this.segundo++;
+      if (this.segundo % 10 == 0)
+        this.multiplo10.emit(this.segundo);
+    }, 1000);
+  }
+
+}
